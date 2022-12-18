df8b21e7588fa2bf234840acf00f316117cc000d
Peng Hao <phao0724@163.com>
2022-12-18 08:42:45 +0800

    Added site '/calculator', and modified site '/home'.

--- main.py
+++ main.py
@@ -7,4 +7,13 @@ app=flask.Flask(__name__)
 def home():
     return flask.render_template("home.html")
 
+@app.route('/calculator', methods=['GET'])
+def calculator():
+    return flask.render_template('calculator.html')
 
+@app.route("/calculator", methods=["POST"])
+def calculate():
+    exp=flask.request.form['exp']
+    return flask.render_template('calculator.html', perexp=exp, pervalue=eval(exp))
+
+app.run()
