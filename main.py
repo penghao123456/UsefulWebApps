6ec6e1a28eb3a83d3f380391963f7c2548afc8a1
Peng Hao <phao0724@163.com>
2022-12-18 16:38:02 +0800

    Fiuxed bugs.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!python
 import flask
+import markdown
 
 app=flask.Flask(__name__)
 
@@ -16,4 +17,18 @@ def calculate():
     exp=flask.request.form['exp']
     return flask.render_template('calculator.html', perexp=exp, pervalue=eval(exp))
 
-app.run()
+@app.route("/markdownrenderer/home", methods=["GET", "POST"])
+def markdownhome():
+    return flask.render_template("/markdownrenderer/home.html")
+
+@app.route("/markdownrenderer/value", methods=["POST"])
+def markdownrender():
+    uploadmethod=flask.request.form["uploadmethod"]
+    if uploadmethod=="text":
+        rendertext=flask.request.form["markdown"]
+    else:
+        rendertext=flask.request.files["file-upload"].read().decode('utf-8')
+    renderresult=markdown.markdown(rendertext)
+    return flask.render_template("/markdownrenderer/value.html", renderfile=rendertext, renderresult=renderresult)
+
+app.run(debug=True)
